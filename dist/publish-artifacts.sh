bd6bd73ff0447f4efcf124d87ca50eca8b7f5556
Martin Kjellstrand <mad@netilium.org>
2018-06-07 18:18:18 +0200

    updates.

--- dist/publish-artifacts.sh
+++ dist/publish-artifacts.sh
@@ -7,15 +7,16 @@ set
 TMPDR="$(mktemp -d)"
 
 ./dist/make-install-sh.sh
-cp dist/install-sh "${TMPDR}"
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git checkout -b artifacts https://${GH_TOKEN}github.com/BI-Beacon/build-artifacts.git "${TMPDR}"
+git clone -b artifacts https://${GH_TOKEN}@github.com/BI-Beacon/build-artifacts.git "${TMPDR}"
+
+cp dist/install-sh "${TMPDR}/cli"
 
 cd "${TMPDR}"
-git add install-sh
+git add cli/install-sh
 git commit -m 'Automated build' .
 git push
 
