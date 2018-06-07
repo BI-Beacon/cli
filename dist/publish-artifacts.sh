328e4a3aac1ac699b80e87f6cbf62243f57380f9
Martin Kjellstrand <github@madworx.se>
2018-06-07 19:50:39 +0200

    Make changes to master branch publish artifacts. (#4)

--- dist/publish-artifacts.sh
+++ dist/publish-artifacts.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-set -x
+if [ "${TRAVIS_BRANCH}" == "master" ] ; then
+    TMPDR="$(mktemp -d)"
 
-TMPDR="$(mktemp -d)"
+    ./dist/make-install-sh.sh
 
-./dist/make-install-sh.sh
-cp dist/install-sh "${TMPDR}"
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name  "Travis CI"
+    
+    git clone -b artifacts https://${GH_TOKEN}@github.com/BI-Beacon/build-artifacts.git "${TMPDR}"
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+    install -m 755 dist/install-sh "${TMPDR}/cli"
+    install -m 755 beaconcli.sh    "${TMPDR}/cli/beaconcli"
 
-git checkout -b artifacts https://${GH_TOKEN}github.com/BI-Beacon/build-artifacts.git "${TMPDR}"
-
-cd "${TMPDR}"
-git add install-sh
-git commit -m 'Automated build' .
-git push
+    cd "${TMPDR}"
+    git add cli/install-sh cli/beaconcli
+    git commit -m 'Automated build' .
+    git push
+fi
 
